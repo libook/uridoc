eabcaee503c0c2da98441ef13b97988b4cc5a479
libook <libook7@gmail.com>
2019-09-28 16:58:01 +0800

    Sort keys of parameter table

--- src/URIDOM.js
+++ src/URIDOM.js
@@ -24,6 +24,13 @@ class ParameterTable {
                 "value": keyValueString.content.substring(separatorIndex + 1).trim(),
             });
         }
+        this.table.sort((a, b) => {
+            if (a.key > b.key) {
+                return 1;
+            } else {
+                return -1;
+            }
+        });
     }
 }
 
