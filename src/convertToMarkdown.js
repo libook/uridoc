eb2bc8049e790feb6ceed24f957132e8fe3c9282
libook <libook7@gmail.com>
2019-10-01 12:23:51 +0800

    Better markdown format

--- src/convertToMarkdown.js
+++ src/convertToMarkdown.js
@@ -13,7 +13,7 @@
 module.exports = function (uridom) {
     let markdown = '\n---\n';
 
-    markdown += `# ${uridom.method} ${uridom.uri}\n\n`;
+    markdown += `### ${uridom.method} ${uridom.uri}\n\n`;
 
     markdown += `${uridom.description.replace(/\n/gm, '\n\n')}\n\n`;
 
@@ -24,11 +24,12 @@ module.exports = function (uridom) {
      * @return {string}
      */
     function keyValueTable(objectName, objectArray) {
-        let tableString = '';
-        tableString += `### ${objectName} ⮷\n\n`;
-        tableString += ''
-            + 'Name | Description\n'
-            + '---- | -----------\n';
+        let tableString = `
+##### ${objectName}
+
+Name | Description
+---- | -----------
+`;
         for (let line of objectArray) {
             tableString += `${line.key} | ${line.value}\n`;
         }
@@ -37,34 +38,37 @@ module.exports = function (uridom) {
     }
 
     function body2Markdown(body) {
-        return '### BODY ⮷\n\n'
-            + '```javascript\n'
-            + body + '\n'
-            + '```\n\n';
+        return `
+##### Body
+
+\`\`\`javascript
+${body}
+\`\`\`
+`;
     }
 
     if (uridom.request) {
-        markdown += '## REQUEST ⮚\n\n';
+        markdown += '#### Request\n\n';
         if (uridom.request.headers) {
-            markdown += keyValueTable('HEADERS', uridom.request.headers.table);
+            markdown += keyValueTable('Headers', uridom.request.headers.table);
         }
         if (uridom.request.path) {
-            markdown += keyValueTable('PATH', uridom.request.path.table);
+            markdown += keyValueTable('Path', uridom.request.path.table);
         }
         if (uridom.request.query) {
-            markdown += keyValueTable('QUERY', uridom.request.query.table);
+            markdown += keyValueTable('Query', uridom.request.query.table);
         }
         if (uridom.request.body) {
             markdown += body2Markdown(uridom.request.body.bodyContent);
         }
     }
     if (uridom.response) {
-        markdown += '## RESPONSE ⮘\n\n';
+        markdown += '#### Response\n\n';
         if (uridom.response.headers) {
-            markdown += keyValueTable('HEADERS', uridom.request.headers.table);
+            markdown += keyValueTable('Headers', uridom.request.headers.table);
         }
         if (uridom.response.body) {
-            markdown += body2Markdown(uridom.response.body.bodyContent)
+            markdown += body2Markdown(uridom.response.body.bodyContent);
         }
     }
     return markdown;
